d301915fad5cd443d83faf9b0100884de829e9fb
Urban Andersson <urban.andersson@chalmers.se>
2026-04-16 09:53:02 +0200

    Added sample images, added systematic_card flag

--- ocr.py
+++ ocr.py
@@ -25,23 +25,24 @@ def ocr_with_claude(image_path):
                 },
                 {
                     "type": "text",
-                    "text": """This is a scanned type written library catalog card with hand written notes etc.
+                    "text": """This is a scanned type written library catalog card with hand written notes.
 Please OCR the text and extract these fields if present:
-- author (surnames are often underlined and following the first name, "/" is often used to mark calling name, / should be excluded in response)
+- authors (surnames are often underlined and sometimes following the first name, "/" is often used to mark calling name, / should be excluded in response)
 - title
 - place_of_publication
 - publisher
 - year_of_publication
-- pages
+- pages (often preceded or followed by "s." or "sidor" or or "s" or "p" or "pp")
 - source_publication (usually preceded by "in:" or "i:" or "ur:")
 - series (often preceded by "serie" or "=")
 - isbn
-- call_number (usually top right corner)
-- classification_code (usually bottom left corner)
+- call_number (usually in the top right corner, normally hand written, usually a short combination of letters and numbers, often with a space in between, e.g., "XZ 1234", sometimes preceded by "+")
 - subject_headings (as a list if there are multiple)
+- classification_code (usually located in the bottom left corner, normally in Universal Decimal Classification (UDC) format (notation, e.g., 000.123.456), sometimes beginning with aphabetic characters, e.g. "Sv.", usually typed)
 - reference (usually preceded by "se" or "ref:")
 - notes
 - bottom_note (bottom center, usually an acronym, i.e. "vb" or "kh")
+- systematic_card, 1 or 0, return 1 if the card only consists of a single call number and short description (otherwise return 0)
 
 Return as JSON only, no explanation."""
                 }
